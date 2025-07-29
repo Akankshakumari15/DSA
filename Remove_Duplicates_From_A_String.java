402a86fdd5407bbcf6b42294c80b95f702f5458c
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-07-29 22:43:00 +0530

    DSA

---
+++ Remove_Duplicates_From_A_String.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Remove_Duplicates_From_A_String {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        // input from user
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        sc.close();
+
+        String newStr ="";
+        for(int i =0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(newStr.indexOf(ch)==-1){
+                newStr=newStr+ch;
+            }
+        }
+        System.out.println(newStr+" ");
+   }
+}
